1619e72f864287a9c3eed5986c18f91da1b3aaa1
root <root@vmi246708.contaboserver.net>
2019-04-12 12:10:42 +0200

    Fixed dereference of registers

--- uafinator.py
+++ uafinator.py
@@ -7,16 +7,16 @@ free_map = {}
 
 
 class FreeHandler(angr.SimProcedure):
-    def run(self):
+    def run(self, ptr):
         caller_address = self.state.addr
-        free_ptr = str(self.state.regs.rdi)
+        free_ptr = hex(self.state.solver.eval(self.state.regs.rdi))
         print("Free called on: %s" % (free_ptr))
         if not free_ptr in free_map:
             free_map[free_ptr] = caller_address
         else:
             print(
                 "Potential Double Free: %s is trying to free %s, which has already been freed by %s"
-                % (caller_address, free_ptr, free_map[free_ptr])
+                % (hex(caller_address), free_ptr, hex(free_map[free_ptr]))
             )
 
 
@@ -24,20 +24,14 @@ def validate_read(state):
     region = state.inspect.mem_read_address
     if region in free_map:
         free_call = free_map.get(region)
-        print(
-            "Potential UAF: %s read from memory freed by %s"
-            % (region, free_call)
-        )
+        print("Potential UAF: %s read from memory freed by %s" % (region, free_call))
 
 
 def validate_write(state):
     region = state.inspect.mem_write_address
     if region in free_map:
         free_call = free_map.get(region)
-        print(
-            "Potential UAF: %s wrote to memory freed by %s"
-            % (region, free_call)
-        )
+        print("Potential UAF: %s wrote to memory freed by %s" % (region, free_call))
 
 
 project.hook_symbol("free", FreeHandler())
