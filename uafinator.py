a0631eef912781462da0407d7b3a0960488bcf8c
root <root@vmi246708.contaboserver.net>
2019-04-12 12:12:17 +0200

    Refactored spurious hex()'s

--- uafinator.py
+++ uafinator.py
@@ -8,7 +8,7 @@ free_map = {}
 
 class FreeHandler(angr.SimProcedure):
     def run(self, ptr):
-        caller_address = self.state.addr
+        caller_address = hex(self.state.addr)
         free_ptr = hex(self.state.solver.eval(self.state.regs.rdi))
         print("Free called on: %s" % (free_ptr))
         if not free_ptr in free_map:
@@ -16,7 +16,7 @@ class FreeHandler(angr.SimProcedure):
         else:
             print(
                 "Potential Double Free: %s is trying to free %s, which has already been freed by %s"
-                % (hex(caller_address), free_ptr, hex(free_map[free_ptr]))
+                % (caller_address, free_ptr, free_map[free_ptr])
             )
 
 
@@ -24,14 +24,20 @@ def validate_read(state):
     region = state.inspect.mem_read_address
     if region in free_map:
         free_call = free_map.get(region)
-        print("Potential UAF: %s read from memory freed by %s" % (region, free_call))
+        print(
+            "Potential UAF: %s read from memory freed by %s"
+            % (region, free_call)
+        )
 
 
 def validate_write(state):
     region = state.inspect.mem_write_address
     if region in free_map:
         free_call = free_map.get(region)
-        print("Potential UAF: %s wrote to memory freed by %s" % (region, free_call))
+        print(
+            "Potential UAF: %s wrote to memory freed by %s"
+            % (region, free_call)
+        )
 
 
 project.hook_symbol("free", FreeHandler())
